bf3df79967ecc4c5931c82716cdd40ede996f1f3
jurre <jurrestender@hotmail.com>
2014-02-25 23:49:10 +0100

    annotate Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      default(""), not null
+#  account_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Project < ActiveRecord::Base
   validates :name, presence: true
   validates :account, presence: true
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      default(""), not null
+#  account_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
