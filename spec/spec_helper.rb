6cf92a8191cd6f82e502b4a23e5b6b16d2131c56
jurre <jurrestender@hotmail.com>
2014-02-25 11:43:33 +0100

    treat symbols as metadata keys with true values for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,7 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.use_transactional_fixtures = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
 Capybara.javascript_driver = :webkit
