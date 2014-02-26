1183141fa520d08a86a9fd1efb696d18d13e0fb9
jurre <jurrestender@hotmail.com>
2014-02-26 21:30:25 +0100

    category names are unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,12 @@ require "spec_helper"
 describe Category do
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name }
+
+    it "should validate case insensitive uniqueness" do
+      create(:category, name: "software development")
+      expect(build(:category, name: "Software Development")).to_not be_valid
+    end
   end
 
   describe "associations" do
