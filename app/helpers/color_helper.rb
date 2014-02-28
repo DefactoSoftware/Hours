8a9cdc3279c6b6b536ef1e94b7cc2be27db1a50c
jurre <jurrestender@hotmail.com>
2014-02-28 16:13:31 +0100

    hash strings manually to unify colors between implementations

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -4,12 +4,21 @@ module ColorHelper
     total_offset = 164
     hex_value = [
       0,
-      string.hash % total_offset,
-      string.hash & total_offset,
+      hash(string) % total_offset,
+      hash(string) & total_offset,
       total_offset
     ].sort.each_cons(2).map do |a, b|
       "%02x" % (start_color + b - a)
     end.join
     "#" + hex_value
+  end
+
+  private
+
+  def hash(string)
+    string.split("").reduce do |memo, obj|
+      hash = ((memo.ord << 5) - memo.ord) + obj.ord
+      (hash & hash).abs
+    end
   end
 end
