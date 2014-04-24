397ac0353ada545d5dc0bbb098a3e45d7cccf0e0
jurre <jurrestender@hotmail.com>
2014-04-24 18:22:14 +0200

    remove reports
    those were only implemented to demonstrate how flexible this data
    is but people seem to think that this is some sort of final
    functionality and it is just not very suited for that.

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Hours::Application.routes.draw do
     resources :users, only: [] do
       resources :entries, only: :index
     end
-    resources :reports, only: [:index]
 
     namespace :api, defaults: {format: :json} do
       resources :entries, only: :index
