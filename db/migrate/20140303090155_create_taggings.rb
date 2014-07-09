0ec017c10b83a3da4fbcd9c2acda112d9a24fdc7
Marcel Horlings <fatboypunk@gmail.com>
2014-07-09 16:29:58 +0200

    update to rails 4.1.4 and rspec 3.0

--- db/migrate/20140303090155_create_taggings.rb
+++ db/migrate/20140303090155_create_taggings.rb
@@ -1,8 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
-      t.references :tag, index: true, null: false
-      t.references :entry, index: true, null: false
+      t.references :tag, index: true
+      t.references :entry, index: true
 
       t.timestamps
     end
