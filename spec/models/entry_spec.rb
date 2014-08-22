ceba23ad2c3644a6feb5e24cb56a2a8442383bf7
jurre <jurrestender@hotmail.com>
2014-08-22 11:52:37 +0200

    validate that hours are integers (fix #168)

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -22,6 +22,7 @@ describe Entry do
     it { should validate_presence_of :hours }
     it { should validate_presence_of :date }
     it { should validate_numericality_of(:hours).is_greater_than(0) }
+    it { should validate_numericality_of(:hours).only_integer }
   end
 
   describe "associations" do
