d3a9aea9b00093249f2b947125ebf196fb0c8eb1
Marthyn Olthof <Marthyn@live.nl>
2014-08-28 10:29:38 +0200

    implement tags overview

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -30,4 +30,52 @@ class Tag < ActiveRecord::Base
   def slug_source
     name
   end
+
+  def hours_per_user
+    users.map do |user|
+      {
+        value: hours_spent_on_user(user),
+        color: user.full_name.pastel_color,
+        label: user.full_name,
+        highlight: "gray"
+      }
+    end
+  end
+
+  def hours_per_project
+    projects.map do |project|
+      {
+        value: hours_spent_on_project(project),
+        color: project.name.pastel_color,
+        highlight: "gray",
+        label: project.name
+      }
+    end
+  end
+
+  def hours_spent_on_user(user)
+    entries.where(user: user).sum(:hours)
+  end
+
+  def hours_spent_on_project(project)
+    entries.where(project: project).sum(:hours)
+  end
+
+  def percentage_spent_on_project(project)
+    hours_spent_on_project(project).to_f / hours_spent.to_f * 100
+  end
+
+  def percentage_spent_on_user(user)
+    hours_spent_on_user(user).to_f / hours_spent.to_f * 100
+  end
+
+  def hours_spent
+    entries.sum(:hours)
+  end
+
+  private
+
+  def slug_source
+    name
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -26,4 +26,71 @@ describe Tag do
     it { should have_many :taggings }
     it { should have_many :entries }
   end
+
+  let(:tag) { create(:tag) }
+  let(:project) { create(:project) }
+  let(:user) { create(:user) }
+
+  describe "#hours_for" do
+    context "with no entries" do
+      it "returns 0" do
+        expect(tag.hours_for(project)).to eq(0)
+      end
+    end
+
+    context "with entries" do
+      it "returns the hours spent" do
+        create(:entry, hours: 2, project: project).tags << tag
+        create(:entry, hours: 3, project: project).tags << tag
+        expect(tag.hours_for(project)).to eq(5)
+      end
+    end
+  end
+
+  describe "#percentage_spent_on_user" do
+    context "with entries with this user" do
+      it "returns the percentage of hours spent" do
+        create(:entry, hours: 2, project: project, user: user).tags << tag
+        create(:entry, hours: 2, project: project).tags << tag
+        expect(tag.percentage_spent_on_user(user)).to eq(50)
+      end
+    end
+  end
+
+  describe "#percentage_spent_on_project" do
+    context "with entries in this project" do
+      it "returns the percentage of hours spent" do
+        create(:entry, hours: 2, project: project).tags << tag
+        create(:entry, hours: 2).tags << tag
+        expect(tag.percentage_spent_on_project(project)).to eq(50)
+      end
+    end
+  end
+
+  describe "#hours_spent" do
+    context "with no entries" do
+      it "returns 0" do
+        expect(tag.hours_spent).to eq(0)
+      end
+    end
+
+    context "with entries tagged" do
+      it "returns the hours spent" do
+        create(:entry, hours: 2, project: project).tags << tag
+        create(:entry, hours: 3, project: project).tags << tag
+        expect(tag.hours_spent).to eq(5)
+      end
+    end
+
+    context "with only part of entries tagged" do
+      it "returns the hours spent" do
+        create(:entry, hours: 3).tags << tag
+        create(:entry, hours: 4).tags << tag
+        create(:entry, hours: 3)
+        create(:entry, hours: 2)
+        expect(tag.hours_spent).to eq(7)
+      end
+    end
+  end
+
 end
