078fb266a07908485241e44f12d1cb489b50f855
Maarten Jacobs <m.jacobs@defacto.nl>
2015-02-23 19:46:58 +0100

    update according to new import file

--- lib/clockwise_import.rb
+++ lib/clockwise_import.rb
@@ -1,4 +1,4 @@
-require "csv"
+require 'csv'
 
 class ClockwiseImporter
   def initialize(csv_path:, tenant:)
@@ -7,11 +7,14 @@ class ClockwiseImporter
   end
 
   def import!
-    Apartment::Tenant.switch tenant
+    Apartment::Tenant.switch @tenant
     Entry.transaction do
-      CSV.foreach(@csv_path, headers: true) do |r|
-        row = ImportRow.new(r[0].split(";"))
-        EntryBuilder.new(row).create!
+      CSV.foreach(@csv_path,
+                  headers: true,
+                  quote_char: '"',
+                  col_sep: ';') do |r|
+        row = ImportRow.new(r)
+        EntryBuilder.new(row).create! unless row.hours < 1
       end
     end
   end
@@ -34,29 +37,31 @@ class EntryBuilder
       hours: @row.hours,
       date: @row.date,
       description: @row.description,
-      billed: @row.billed
     )
   end
 
   private
 
   def build_client
-    Client.find_or_create_by(name: @row.client)
+    Client.where('lower(name) = ?', @row.client)
+      .first_or_create(name: @row.client)
   end
 
   def build_project(client)
-    Project.find_or_create_by(name: @row.project) do |project|
-      project.billable = true
-      project.client = client
-    end
+    Project.where('lower(name) = ?', @row.project.downcase)
+      .first_or_create(name: @row.project,
+                       billable: true,
+                       client: client,
+                       archived: @row.archive)
   end
 
   def build_category
-    Category.find_or_create_by(name: @row.category)
+    Category.where('lower(name) = ?', @row.category.downcase)
+      .first_or_create(name: @row.category)
   end
 
   def build_user
-    names = @row.user.split(" ")
+    names = @row.user.split(' ')
     first_name = names.first
     last_name = names.last
     full_name = { first_name: first_name, last_name: last_name }
@@ -68,13 +73,13 @@ class EntryBuilder
   end
 
   def build_email_from_name(name)
-    names = name.split(" ")
+    names = name.split(' ')
     "#{names[0][0]}.#{names[1]}@defacto.nl".downcase
   end
 end
 
 class ImportRow
-  attr_reader :billed,
+  attr_reader :archive,
               :user,
               :client,
               :project,
@@ -84,21 +89,25 @@ class ImportRow
               :description
 
   def initialize(row)
-    @billed = row[0] == "inactief"
+    @archive = row[0] == 'J'
     @user = row[1]
     @client = row[2]
-    @project = row[3]
+    if row[3].to_s.empty?
+      @project = row[2]
+    else
+      @project = row[3]
+    end
     @category = row[4]
     @date = parse_date(row[7])
-    @hours = Integer(row[8])
+    @hours = row[8].to_s.gsub(',','.').to_f.round
     @description = String(row[9]).truncate(255)
   end
 
   private
 
   def parse_date(date_string)
-    comps = date_string.split("/")
-    year = Integer("20" + comps[2].last(2))
+    comps = date_string.split('/')
+    year = Integer('20' + comps[2].last(2))
     month = Integer(comps[1])
     day = Integer(comps[0])
     DateTime.new(year, day, month)
