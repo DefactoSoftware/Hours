42dafa93d85589fcb56d020f4398e7246d5e6578
Maarten Jacobs <m.jacobs@defacto.nl>
2015-04-09 16:04:06 +0200

    just reset session on invalid authenticitytoken instead of an exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :reset_session
   before_filter :load_schema, unless: -> { Hours.single_tenant_mode? }
   before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
