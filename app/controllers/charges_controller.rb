88824bce7f65e83d350a1bbb87fe95ac6102cfad
Marcel Horlings <marcel.horlings@gmail.com>
2015-06-26 10:44:24 +0200

    Do not work when in single tenant mode or without stripe setup

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,19 @@
 class ChargesController < ApplicationController
+  before_action :chargin_set?
+
   def new
   end
 
   def create
-    # Get the credit card details submitted by the form
     token = params[:stripeToken]
 
-    # Create a Customer
     customer = Stripe::Customer.create(
       source: token,
       plan: "value",
       email: current_user.email,
       quantity: user_count
     )
+    pp customer.instance_methods(false)
     current_account.update(stripe_id: customer.id)
   end
 
@@ -21,5 +22,10 @@ class ChargesController < ApplicationController
   def user_count
     @user_count ||= User.count
   end
-end
 
+  def chargin_set?
+    Hours.single_tenant_mode? == false &&
+      ENV["STRIPE_PUBLIC_KEY"] &&
+      ENV["STRIPE_SECRET_KEY"]
+  end
+end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,10 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV["STRIPE_PUBLIC_KEY"],
-  secret_key: ENV["STRIPE_SECRET_KEY"]
-}
+if Hours.single_tenant_mode? == false &&
+   ENV["STRIPE_PUBLIC_KEY"] &&
+   ENV["STRIPE_SECRET_KEY"]
+  Rails.configuration.stripe = {
+    publishable_key: ENV["STRIPE_PUBLIC_KEY"],
+    secret_key: ENV["STRIPE_SECRET_KEY"]
+  }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end
