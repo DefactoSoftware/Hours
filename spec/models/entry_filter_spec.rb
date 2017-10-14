ed3184bc5843c7a73f8318995b819e7b01ff2434
Andy Stabler <andy_60@hotmail.co.uk>
2017-10-14 23:22:21 +0100

    Add missing tests

--- spec/models/entry_filter_spec.rb
+++ spec/models/entry_filter_spec.rb
@@ -23,12 +23,54 @@ describe EntryFilter do
     filter.projects
   end
 
-  it "billed_options" do
+  it "#billed_options" do
     filter = EntryFilter.new
 
     expect(filter.billed_options).to eq([
       [I18n.t("entry_filters.not_billed"), false],
       [I18n.t("entry_filters.billed"), true]
     ])
+  end
+
+  it "#billable_options" do
+    filter = EntryFilter.new
+
+    expect(filter.billable_options).to eq([
+      [I18n.t("entry_filters.not_billable"), false],
+      [I18n.t("entry_filters.billable"), true]
+    ])
+  end
+
+  it "#archived_options" do
+    filter = EntryFilter.new
+
+    expect(filter.archived_options).to eq([
+      [I18n.t("entry_filters.not_archived"), false],
+      [I18n.t("entry_filters.archived"), true]
+    ])
+  end
+
+  describe "#from_date" do
+    it "is a DateTime" do
+      filter = EntryFilter.new(from_date: "14/10/2017")
+      expect(filter.from_date).to eq DateTime.parse("14/10/2017")
+    end
+
+    it "is nil when there is no from_date" do
+      filter = EntryFilter.new
+      expect(filter.from_date).to be_nil
+    end
+  end
+
+  describe "#to_date" do
+    it "is a DateTime" do
+      filter = EntryFilter.new(to_date: "14/10/2017")
+      expect(filter.to_date).to eq DateTime.parse("14/10/2017")
+    end
+
+    it "is nil when there is no to_date" do
+      filter = EntryFilter.new
+      expect(filter.to_date).to be_nil
+    end
   end
 end
