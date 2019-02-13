3410653bdfd5d094e574327da0845d00fdb128d2
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2019-02-13 01:09:33 +0100

    remove unused files

--- spec/controllers/send_csv_to_third_party_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SendCsvToThirdPartyController, type: :controller do
-
-end
--- spec/mailers/previews/third_party_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/third_party_mailer
-class ThirdPartyMailerPreview < ActionMailer::Preview
-
-end
